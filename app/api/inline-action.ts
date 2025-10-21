7667196faff1e421d0e4d020fabf83056d750e97
MOΞ <moe.salih@gmail.com>
2025-10-21 14:54:07 -0400

    sample return embed

--- app/api/inline-action.ts
+++ app/api/inline-action.ts
@@ -3,7 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
 
-    res.status(200).json({ message: 'test' });
+    res.status(200).json({
+      message: 'test',
+      success: true,
+      embed: {
+        imageUrl: 'https://imgur.com/NgvRfSF.png',
+        buttons: [
+          {
+            title: 'Event Details...',
+            url: 'https://frames-v2-demo-lilac.vercel.app/test?answer=true&payment=false',
+            variant: 'outlined',
+          },
+        ],
+
+      },
+    });
 
 
   } else {
