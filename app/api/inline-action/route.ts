bfa120bc8769bf9b25e351cd893a25f3f5cad763
MOΞ <moe.salih@gmail.com>
2025-10-21 16:10:22 -0400

    log req body

--- app/api/inline-action/route.ts
+++ app/api/inline-action/route.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
 
+  const body = await request.json();
+  console.log(body)
+
   return Response.json({
     message: 'test',
     success: true,
