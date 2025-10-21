c377aedd2f3cf1c928f1ca97af88e92791704678
MOΞ <moe.salih@gmail.com>
2025-10-21 14:58:05 -0400

    fix route

--- app/api/inline-action/route.ts
+++ app/api/inline-action/route.ts
@@ -1,26 +1,21 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from "next/server";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export async function POST(request: NextRequest) {
 
-    res.status(200).json({
-      message: 'test',
-      success: true,
-      embed: {
-        imageUrl: 'https://imgur.com/NgvRfSF.png',
-        buttons: [
-          {
-            title: 'Event Details...',
-            url: 'https://frames-v2-demo-lilac.vercel.app/test?answer=true&payment=false',
-            variant: 'outlined',
-          },
-        ],
+  Response.json({
+    message: 'test',
+    success: true,
+    embed: {
+      imageUrl: 'https://imgur.com/NgvRfSF.png',
+      buttons: [
+        {
+          title: 'Event Details...',
+          url: 'https://frames-v2-demo-lilac.vercel.app/test?answer=true&payment=false',
+          variant: 'outlined',
+        },
+      ],
 
-      },
-    });
+    },
+  });
 
-
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
 }
