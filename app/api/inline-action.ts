b55c186394e127b4e9bef72ae6c6a50c004eb1be
MOΞ <moe.salih@gmail.com>
2025-10-21 14:51:19 -0400

    api route

---
+++ app/api/inline-action.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+
+    res.status(200).json({ message: 'test' });
+
+
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
