f8ac917ac64304c2910a0a4bf271ce5ea026b1d1
MOΞ <moe.salih@gmail.com>
2025-10-22 10:33:57 -0400

    styling

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Farcaster Fridays</h1>
         <div className="flex flex-col gap-8 items-center sm:items-start">
